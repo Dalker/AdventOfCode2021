f81983adc97c0465ccd9a4264cc24a70e50f0262
Daniel Kessler (a.k.a. Dalker) <daniel.kessler@hornord.net>
2021-12-01 18:46:18 +0100

    ready for tomorrow!

---
+++ aoc_02.py
@@ -0,0 +1,20 @@
+"""
+Advent of Code - tentative pour J2.
+
+Daniel Kessler (aka Dalker), le 2021.12.02
+"""
+
+
+def get_data(day) -> list[int]:
+    """Read the day's input file and return contents as a list of ints."""
+    with open(f"input{day}.txt") as datafile:
+        data = [int(line) for line in datafile]
+    return data
+
+
+if __name__ == "__main__":
+    data = get_data("02")
+    print("check hint 1:", )
+    print("check hint 2:", )
+    print("  solution 1:", )
+    print("  solution 2:", )
