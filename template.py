787b682f896a0059d9756462e45bee68665a6ac7
Daniel Kessler (a.k.a. Dalker) <daniel.kessler@hornord.net>
2021-12-01 18:37:41 +0100

    add template, reusable as is if input files are similar in following days

---
+++ template.py
@@ -0,0 +1,20 @@
+"""
+Advent of Code - tentative pour J.
+
+Daniel Kessler (aka Dalker), le 2021.12.
+"""
+
+
+def get_data() -> list[int]:
+    """Read the day's input file and return contents as a list of ints."""
+    with open("input01.txt") as datafile:
+        data = [int(line) for line in datafile]
+    return data
+
+
+if __name__ == "__main__":
+    data = get_data()
+    print("check hint 1:", )
+    print("check hint 2:", )
+    print("  solution 1:", )
+    print("  solution 2:", )
