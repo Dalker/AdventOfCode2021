9fb77fedc39c4cd00e35521785f1a98e22db2d39
Daniel Kessler (a.k.a. Dalker) <daniel.kessler@hornord.net>
2021-12-01 18:44:19 +0100

    make template more generic

--- template.py
+++ template.py
@@ -5,15 +5,15 @@ Daniel Kessler (aka Dalker), le 2021.12.
 """
 
 
-def get_data() -> list[int]:
+def get_data(day) -> list[int]:
     """Read the day's input file and return contents as a list of ints."""
-    with open("input01.txt") as datafile:
+    with open(f"input{day}.txt") as datafile:
         data = [int(line) for line in datafile]
     return data
 
 
 if __name__ == "__main__":
-    data = get_data()
+    data = get_data("01")
     print("check hint 1:", )
     print("check hint 2:", )
     print("  solution 1:", )
