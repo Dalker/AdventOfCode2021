ba7109d44986d9815bd4875a4ad2cebc9ccfb4e6
Daniel Kessler (a.k.a. Dalker) <daniel.kessler@hornord.net>
2021-12-07 21:54:41 +0100

    cleaned up code and combined both solving functions into one

--- aoc_07.py
+++ aoc_07.py
@@ -5,49 +5,34 @@ Daniel Kessler (aka Dalker), le 2021.12.
 """
 
 DAY = "07"
-HINTDATA = "16,1,2,0,4,2,7,1,2,14"
+HINTDATA = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
 
 
-def get_data(fname: str) -> str:
+def get_data(fname: str) -> list[int]:
     """Read the day's input and return contents in adequate data structure."""
-    try:
-        with open(f"{fname}.txt") as datafile:
-            data = datafile.read()
-    except FileNotFoundError:
-        print("Day's data file not found. Using Hint Data instead.")
-        return []
+    with open(f"{fname}.txt") as datafile:
+        data = [int(n) for n in datafile.read().strip().split(",")]
     return data
 
 
-def easy(data: list[str]) -> int:
-    """Easy problem of the day."""
-    data = [int(n) for n in data.strip().split(",")]
-    costs = [0] * (max(data) + 1)
-    for final_position, _ in enumerate(costs):
-        cost = 0
-        for datum in data:
-            cost += abs(final_position - datum)
-        costs[final_position] = cost
-    return min(zip(costs, range(max(data) + 1)))
-
-
-def hard(data: list[str]) -> int:
+def solve(data: list[int], part2: bool = False) -> tuple[int, int]:
     """Hard problem of the day."""
-    data = [int(n) for n in data.strip().split(",")]
     costs = [0] * (max(data) + 1)
     for final_position, _ in enumerate(costs):
         cost = 0
         for datum in data:
-            distance = abs(final_position - datum)
-            cost += (distance * (distance+1)) // 2
+            if not part2:
+                cost += abs(final_position - datum)
+            else:
+                distance = abs(final_position - datum)
+                cost += (distance * (distance+1)) // 2
         costs[final_position] = cost
     return min(zip(costs, range(max(data) + 1)))
 
 
 if __name__ == "__main__":
-    hintdata = HINTDATA
     realdata = get_data(f"input{DAY}")
-    print("check hint 1:", easy(hintdata))
-    print("check hint 2:", hard(hintdata))
-    print("  solution 1:", easy(realdata))
-    print("  solution 2:", hard(realdata))
+    print("check hint 1:", solve(HINTDATA))
+    print("check hint 2:", solve(HINTDATA, part2=True))
+    print("  solution 1:", solve(realdata))
+    print("  solution 2:", solve(realdata, part2=True))
