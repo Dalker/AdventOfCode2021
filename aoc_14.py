4626158b388867fadf91f150f5e1be0111a58fa5
Daniel Kessler (a.k.a. Dalker) <daniel.kessler@hornord.net>
2021-12-14 19:46:00 +0100

    cleanup to make mypy/pylint happy; part 2 still not solvable

--- aoc_14.py
+++ aoc_14.py
@@ -7,7 +7,7 @@ Daniel Kessler (aka Dalker), le 2021.12.14
 import cProfile
 from time import process_time
 import tracemalloc
-from collections import Counter, deque
+from collections import Counter  # , deque
 
 DAY = "14"
 
@@ -32,9 +32,9 @@ def advance(start: str, rules: dict[str, str]):
     return chain
 
 
-def count(chain: str) -> int:
+def count_chain(chain: str) -> int:
     """Count difference between most and least frequent letter."""
-    counts = Counter()
+    counts: dict[str, int] = Counter()
     for char in chain:
         counts[char] += 1
     printcount(counts)
@@ -48,12 +48,12 @@ def solve(base: str, rules: dict[str, str], steps=10) -> int:
     chain = base
     for _ in range(steps):
         chain = advance(chain, rules)
-    return count(chain)
+    return count_chain(chain)
 
 
 def solve2(base: str, rules: dict[str, str], steps=10) -> int:
     """Solve problem of the day in a smarter way (using a lot less memory)."""
-    counts = Counter()
+    counts: dict[str, int] = Counter()
     # essayé avec deque et list en profilant - pas de grande différence
     # et pour deque avec popleft/appendleft et pop/append - idem
     rest = list((char, steps) for char in base)
@@ -80,21 +80,24 @@ class Profiler:
         tracemalloc.start()
 
     def stamp(self):
+        """Print process time and peak mem usage since creation."""
         newtime = process_time()
         print(" --> time (ms):", round(1000*(newtime - self.time)),
               "-- peak mem (kB)", round(tracemalloc.get_traced_memory()[1] / 1000, 1))
         self.time = newtime
-        tracemalloc.reset_peak()
+        # tracemalloc.reset_peak()
 
 
-def printcount(count: Counter):
+def printcount(count: dict[str, int]):
     """Print a Counter in sorted order, for debugging purposes."""
     print(" <", end="")
     print(", ".join([str(key) + ": " + str(count[key])
                      for key in sorted(count)]), end=">\n")
 
 
-def test(name: str, solver, data, steps):
+def test(name: str, solver, data: tuple, steps: int):
+    """Profile a given solver on a given dataset."""
+    profile = Profiler()
     print("** ", name, " **")
     print("solution:", solver(*data, steps))
     profile.stamp()
@@ -103,10 +106,9 @@ def test(name: str, solver, data, steps):
 if __name__ == "__main__":
     hintdata = get_data(f"hintdata{DAY}")
     realdata = get_data(f"input{DAY}")
-    profile = Profiler()
     test("hint, solver 1", solve, hintdata, 10)
     test("hint, solver 2", solve2, hintdata, 10)
     test("part 1, solver 1", solve, realdata, 10)
     test("part 1, solver 2", solve2, realdata, 10)
     # test("part 1 + epsilon, solver 2", solve2, realdata, 20)
-    # cProfile.run("solve2(*realdata, 20)")
+    cProfile.run("solve2(*realdata, 15)")
