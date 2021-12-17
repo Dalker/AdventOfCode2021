4767e6ddaa93b924ce69b59e909d12bc2c1a324d
Daniel Kessler (a.k.a. Dalker) <daniel.kessler@hornord.net>
2021-12-17 19:08:44 +0100

    cleanup of day 17

--- aoc_17.py
+++ aoc_17.py
@@ -6,18 +6,6 @@ Daniel Kessler (aka Dalker), le 2021.12.17
 from dataclasses import dataclass
 from math import ceil
 
-DAY = "17"
-
-
-def get_data(fname: str) -> list[int]:
-    """Read the day's input and return contents in adequate data structure."""
-    with open(f"{fname}.txt") as datafile:
-        line = datafile.readline().strip()
-    ind0, ind1 = line.find("="), line.find(",")
-    xmin, xmax = line[ind0+1:ind1].split("..")
-    ymin, ymax = line[line.find("=", ind1)+1:].split("..")
-    return Area(*(int(n) for n in (xmin, xmax, ymin, ymax)))
-
 
 @dataclass
 class Area:
@@ -28,42 +16,48 @@ class Area:
     bottom: int
 
     def __contains__(self, coords: tuple[int, int]) -> bool:
-        x, y = coords
-        return self.left <= x <= self.right and self.top <= y <= self.bottom
+        return (self.left <= coords[0] <= self.right and
+                self.top <= coords[1] <= self.bottom)
 
 
-class Problem:
-    """Problem of the day."""
+def get_data(fname: str) -> Area:
+    """Read the day's input and return contents in adequate data structure."""
+    with open(f"{fname}.txt") as datafile:
+        line = datafile.readline().strip()
+    ind0, ind1 = line.find("="), line.find(",")
+    xmin, xmax = line[ind0+1:ind1].split("..")
+    ymin, ymax = line[line.find("=", ind1)+1:].split("..")
+    return Area(*(int(n) for n in (xmin, xmax, ymin, ymax)))
 
-    def __init__(self, area: Area):
-        """Solve problem of the day."""
-        self.area = area
-        self.vx0min = ceil((-1 + (1 + 8*self.area.left)**.5/2))
-        self.vy0min = self.area.top
 
-    def solve(self) -> str:
-        y_max = 0
-        n_paths = 0
-        for vx_init in range(self.vx0min, self.area.right + 1):
-            for vy_init in range(self.area.top - 1, self.area.right + 1):
-                vx, vy = vx_init, vy_init
-                x, y = 0, 0
-                current_ymax = 0
-                while x <= self.area.right and y >= self.area.top:
-                    x += vx
-                    y += vy
-                    vx = max(vx - 1, 0)
-                    vy -= 1
-                    current_ymax = max(current_ymax, y)
-                    if (x, y) in self.area:
-                        y_max = max(y_max, current_ymax)
-                        n_paths += 1
-                        break
-        return f"y_max = {y_max:4d} , n_paths = {n_paths:4d}"
+def solve(area) -> str:
+    """Solve both parts of day's problem."""
+    # following was found with some pen and paper maths
+    vx0min = ceil((-1 + (1 + 8*area.left)**.5/2))
+    y_max = 0
+    n_paths = 0
+    for vx_init in range(vx0min, area.right + 1):
+        # following has *not* been optimized but works for *actual data*
+        for vy_init in range(area.top - 1, area.right + 1):
+            # pylint: disable=C0103
+            vx, vy = vx_init, vy_init
+            x, y = 0, 0
+            current_ymax = 0
+            while x <= area.right and y >= area.top:
+                x += vx
+                y += vy
+                vx = max(vx - 1, 0)
+                vy -= 1
+                current_ymax = max(current_ymax, y)
+                if (x, y) in area:
+                    y_max = max(y_max, current_ymax)
+                    n_paths += 1
+                    break
+    return f"y_max = {y_max:4d} , n_paths = {n_paths:4d}"
 
 
 if __name__ == "__main__":
-    hintdata = get_data(f"hintdata{DAY}")
-    realdata = get_data(f"input{DAY}")
-    print("   check hint:", Problem(hintdata).solve())
-    print("solve problem:", Problem(realdata).solve())
+    hintdata = get_data("hintdata17")
+    realdata = get_data("input17")
+    print("   check hint:", solve(hintdata))
+    print("solve problem:", solve(realdata))
