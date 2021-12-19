72397770ad9767338f943892b485d507857640b3
Daniel Kessler (a.k.a. Dalker) <daniel.kessler@hornord.net>
2021-12-19 22:44:57 +0100

    code cleanup

--- aoc_19.py
+++ aoc_19.py
@@ -8,12 +8,9 @@ from __future__ import annotations
 from copy import copy
 from collections import Counter
 from itertools import permutations, product
-from typing import Iterable
 
 import numpy as np
 import numpy.typing as npt
-
-Triplet = [int, int, int]
 
 
 class Scanner:
@@ -22,8 +19,9 @@ class Scanner:
     def __init__(self, identity: str):
         self.identity = identity
         self.beacons: list[tuple[int, ...]] = []
+        self.n_beacons = 0
         self.matrix: npt.NDArray
-        self.location = np.zeros(3)
+        self.location = np.zeros(3, dtype=int)
 
     def __repr__(self) -> str:
         return (self.identity + "(" + ",".join(str(beacon) for beacon in
@@ -32,7 +30,7 @@ class Scanner:
     def post_init(self):
         """Finish construction after all beacons are loaded."""
         self.n_beacons = len(self.beacons)
-        self.matrix = np.zeros((self.n_beacons, 3))
+        self.matrix = np.zeros((self.n_beacons, 3), dtype=int)
         for n_beacon, beacon in enumerate(self.beacons):
             for dim in range(3):
                 self.matrix[n_beacon][dim] = beacon[dim]
@@ -46,10 +44,11 @@ class Scanner:
         for dims, signs in product(permutations(range(3)),
                                    product((-1, +1), repeat=3)):
             diff_freq: dict[tuple[int, ...], int] = Counter()
+            # pylint: disable=C0103
             for m, n in product(range(self.n_beacons), range(other.n_beacons)):
-                diff_freq[tuple([self.matrix[m, i]
-                                 - signs[i] * other.matrix[n, dims[i]]
-                                 for i in range(3)])] += 1
+                diff_freq[tuple(self.matrix[m, i]
+                                - signs[i] * other.matrix[n, dims[i]]
+                                for i in range(3))] += 1
             for diff, freq in diff_freq.items():
                 if freq >= 12:
                     # print(dims, signs, diff)
@@ -61,44 +60,6 @@ class Scanner:
                     other.location += np.array(diff)
                     return True
         return False
-
-    def adjust(self, other: Scanner, dim: int, sign: int, diff: int):
-        """DEPRECATED - Adjust another scanner to show same coords as self."""
-        other.matrix[:, [0, dim]] = other.matrix[:, [dim, 0]]
-        other.matrix[:, 0] *= sign
-        other.matrix[:, 0] -= diff
-        for n, m in product(range(self.n_beacons), range(other.n_beacons)):
-            choices = product((-1, 1), (1, 2))
-            prevdiff = [0] * 4
-            newdiff = [0] * 4
-            if self.matrix[n, 0] == other.matrix[m, 0]:
-                for index, choice in enumerate(choices):
-                    sign, dim = choice
-                    newdiff[index] = self.matrix[n, 1] - sign * other.matrix[m, dim]
-                    if newdiff[index] == prevdiff[index]:
-                        diff = newdiff[index]
-                        break
-                    else:
-                        prevdiff[index] = newdiff[index]
-                other.matrix[:, [1, dim]] = other.matrix[:, [dim, 1]]
-                other.matrix[:, 1] *= sign
-                other.matrix[:, 1] += diff
-                break
-        for n, m in product(range(self.n_beacons), range(other.n_beacons)):
-            signs = (-1, 1)
-            prevdiff = [0] * 2
-            newdiff = [0] * 2
-            if self.matrix[n, 0] == other.matrix[m, 0]:
-                for index, sign in enumerate(signs):
-                    newdiff[index] = self.matrix[n, 2] - sign * other.matrix[m, 2]
-                    if newdiff[index] == prevdiff[index]:
-                        diff = newdiff[index]
-                        break
-                    else:
-                        prevdiff[index] = newdiff[index]
-                other.matrix[:, 2] *= sign
-                other.matrix[:, 2] += diff
-                break
 
 
 def get_data(fname: str) -> list[Scanner]:
@@ -120,13 +81,13 @@ def get_data(fname: str) -> list[Scanner]:
 
 def longest_distance(scanners: list[Scanner]) -> int:
     """Find longest Manhattan distance between two scanners."""
-    def manhattan(a: Iterable, b: Iterable):
+    def manhattan(a: npt.NDArray, b: npt.NDArray):  # pylint: disable=C0103
         return sum(abs(a[i] - b[i]) for i in range(len(a)))
     return max(manhattan(s1.location, s2.location)
                for s1, s2 in permutations(scanners, 2))
 
 
-def solve(scanners: list[Scanner]) -> int:
+def solve(scanners: list[Scanner]) -> tuple[int, int]:
     """Solve problem of the day."""
     not_adjusted = list(range(1, len(scanners)))
     queue = [0]
