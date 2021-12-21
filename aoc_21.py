0928d3428d0b4c184501518e463f0fbc12723c99
Daniel Kessler (a.k.a. Dalker) <daniel.kessler@hornord.net>
2021-12-21 16:11:36 +0100

    Day 21 done

--- aoc_21.py
+++ aoc_21.py
@@ -36,7 +36,7 @@ def warmup(positions: list[int]) -> int:
         scores[turn] += positions[turn] + 1
         turn = (turn+1) % 2
         rolls += 3
-    print(scores, rolls)
+    # print(scores, rolls)
     return min(scores) * rolls
 
 
@@ -46,17 +46,19 @@ RESULT_COUNT = {3: 1, 4: 3, 5: 6, 6: 7, 7: 6, 8: 3, 9: 1}
 
 def dirac(positions: list[int]) -> int:
     """Solve the real problem of the day."""
-    # universe = {(pos0-1, pos1-1, score0, score1): count}
-    universes = {(positions[0]-1, positions[1]-1, 0, 0): 1}
+    # universe = {(pos0-1, pos1-1, score0, score1, history): count}
+    # it seems that history matters...
+    universes = {(positions[0]-1, positions[1]-1, 0, 0, 0): 1}
     turn = 0  # whose player's turn it is to play next
     while any(score[2] < 21 and score[3] < 21 for score in universes):
         new_universes = Counter()  # nb universes for each new configuration
         for universe, count1 in universes.items():
-            pos0, pos1, score0, score1 = universe
+            pos0, pos1, score0, score1, history = universe
             if score0 >= 21 or score1 >= 21:
                 # someone already won in this configuration
                 new_universes[universe] = count1
                 continue
+            history = history*10 + pos0
             for result, count2 in RESULT_COUNT.items():
                 # player who has turn moves by result of 3 dirac dice rolls
                 new_count = count1 * count2
@@ -64,12 +66,14 @@ def dirac(positions: list[int]) -> int:
                     new_pos0 = (pos0 + result) % 10
                     new_score0 = score0 + new_pos0 + 1
                     new_universes[(new_pos0, pos1,
-                                   new_score0, score1)] += new_count
+                                   new_score0, score1,
+                                   history)] += new_count
                 else:
                     new_pos1 = (pos1 + result) % 10
                     new_score1 = score1 + new_pos1 + 1
                     new_universes[(pos0, new_pos1,
-                                   score0, new_score1)] += new_count
+                                   score0, new_score1,
+                                   history)] += new_count
         universes = new_universes
         turn = (turn+1) % 2
     wins0 = sum(count for score, count in universes.items() if score[2] >
@@ -85,3 +89,4 @@ if __name__ == "__main__":
     print("check hint 1:", warmup(hintdata))
     print("  solution 1:", warmup(realdata))
     print("check hint 2:", dirac(hintdata))
+    print("  solution 2:", dirac(realdata))
